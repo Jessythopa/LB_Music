f78ce13ac992985c981e3d4a7c74a0f9c4d40d31
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-06-10 23:27:16 +0530

    Update welcome.py

--- SlayerX/plugins/tools/welcome.py
+++ SlayerX/plugins/tools/welcome.py
@@ -109,7 +109,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("SlayerX/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((400, 400))
+    pfp = pfp.resize((200, 200))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('SlayerX/assets/font.ttf', size=70)
     welcome_font = ImageFont.truetype('SlayerX/assets/font.ttf', size=61)
